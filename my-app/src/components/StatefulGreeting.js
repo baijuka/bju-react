e69448da95241c19c87ee49357ce915556214081
baijuka <baijuka@yahoo.com>
2022-06-27 23:28:40 +0000

    update StatefullGreeting component

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,12 +7,24 @@ class StatefulGreeting extends React.Component {
             this.state = {
                 introduction: 'Hello',
                 buttonText: 'Exit',
+                count: 0,
             }
         }
 
     handleClick() {
-        this.setState({
-            introduction: 'Goodbye!',
+        this.setState((prevState, prevProps) => {
+            return {
+                introduction: prevState.introduction ==='Hello' ? 'Goodbye' : 'Hello',
+                buttonText: prevState.buttonText === 'Exit' ? 'Enter' : 'Exit' ,
+            }
+        });
+    }
+
+    increment() {
+        this.setState((prevState, prevProps)=>{   
+          return {
+            count: prevState.count + 1,
+          } 
         });
     }
 
@@ -20,7 +32,9 @@ class StatefulGreeting extends React.Component {
         return (
         <div>
         <h1>{this.state.introduction} {this.props.name}, {this.props.greeting} </h1>
+        <h2>Count: {this.state.count}</h2>
         <button onClick={()=>this.handleClick()}>{this.state.buttonText}</button>
+        <button onClick={()=>this.increment()}>+</button>
         </div>
         )
     }
